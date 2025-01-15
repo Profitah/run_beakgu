81e6f4bb446bc0a15fca8fe4f4468311cacaaf88
이윤아 <qwer120542@gmail.com>
2025-01-16 02:28:06 +0900

    chore: #0 프로젝트 초기 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+export const eslintExtends = [
+  'eslint:recommended',
+  'plugin:react/recommended',
+  'plugin:@next/next/recommended',
+  'plugin:prettier/recommended',
+  rules,
+  {
+    'prettier/prettier': 'warn',
+  },
+];
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <div>초기설정 커밋을 위해 작성하는 페이지입니다.</div>
+    </div>
+  );
+};
+
+export default index;
